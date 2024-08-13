7231d20c6e199e0ffea789f7092b3a52beddf5e9
ZAKARIA TOURAS <zakariatouras47@gmail.com>
2024-08-12 20:03:01 -0700

    fixed 1

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const my_amenities = {};
+  $('input[type="checkbox"]').change(function () {
+  const data_id = $(this).attr('data-id');
+  const data_name = $(this).attr('data-name');
+  if ($(this).is(':checked')) {
+    my_amenities[data_id] = data_name;
+  } else {
+    delete my_amenities[data_id];
+  }
+  const amt_list = [];
+  for (const key in my_amenities) {
+    amt_list.push(my_amenities[key]);
+  }
+  $('.amenities h4').text(amt_list.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (content) {
+    if (content.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+    $('div#api_status').removeClass('available');
+    }
+  });
+});
